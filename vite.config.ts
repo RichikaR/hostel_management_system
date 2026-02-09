060fbaaa465b60ebc6e98a2b96e2cd18221fb7d5
RichikaR <richikarani20@gmail.com>
2026-02-09 21:40:16 +0530

    hosting changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // The 'base' should be set to your repository name to work on GitHub Pages
+  base: "/hostel_management_system/", 
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +14,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(), 
+    mode === "development" && componentTagger()
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
